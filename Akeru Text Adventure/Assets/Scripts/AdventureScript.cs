8cda795e5b13c95822f1f6dcc68338663ba83802
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2019-02-02 19:18:29 +0000

    added scriptableObject for states

--- Akeru Text Adventure/Assets/Scripts/AdventureScript.cs
+++ Akeru Text Adventure/Assets/Scripts/AdventureScript.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AdventureScript : MonoBehaviour {
 
-	[SerializeField] private Text textField; 
+	[SerializeField] private Text textField;
+
+	[SerializeField] private State startingState;
+
+	private State currentState; 
 	// Use this for initialization
 	void Start () {
 
-		textField.text = "Welcome to the airship Akeru. One of the last ships capable of descending below the corona, the thick haze that now covers the surface. Nobody knows why" +
-		                 " the Carona formed, some say it was the result of the experiments of men, other the natural defence of the planate below in response to man's disregard " +
-		                 "for it. Whatever the case the one thing that everyone can agree on is that things lurk in the corona. Things only seen in the storybooks of old. Many ha" +
-		                 "ve tried to venture below the corona, very few have returned. Now it is your turn Adventurer. Perhaps you will succeed where so many have failed. Good look. "; 
+		currentState = startingState; 
+		
+
+		textField.text = currentState.getStoryText(); 
 	}
 	
 	// Update is called once per frame
