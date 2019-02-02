4a9c049d69f4c325f7b219307e5ff7b91304eaac
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2019-02-02 19:19:31 +0000

    added scriptableObject for states

---
+++ Akeru Text Adventure/Assets/Scripts/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "State")]
+public class State : ScriptableObject {
+
+    
+    private string storyText;
+
+    public string getStoryText() {
+        return storyText;
+    }
+}
